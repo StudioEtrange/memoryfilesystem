7309cceacdf771c443aa497ca471c4a317a96f79
StudioEtrange <sboucault@gmail.com>
2015-12-07 10:15:16 +0100

    64k for block size

--- src/test/java/com/github/marschall/memoryfilesystem/MemoryFileTest.java
+++ src/test/java/com/github/marschall/memoryfilesystem/MemoryFileTest.java
@@ -26,13 +26,13 @@ public class MemoryFileTest {
   @Test
   public void directBlock() {
     byte[] array = new byte[MemoryFile.BLOCK_SIZE];
-    assertEquals(16384, VMSupport.sizeOf(array));
+    assertEquals(65536, VMSupport.sizeOf(array));
   }
 
   @Test
   public void indirectBlock() {
     byte[][] array = new byte[MemoryFile.BLOCK_SIZE][];
-    assertThat(VMSupport.sizeOf(array), lessThanOrEqualTo(16384*16384));
+    assertThat(VMSupport.sizeOf(array), lessThanOrEqualTo(65536*65536));
   }
 
   @Test
